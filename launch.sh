e11437709351ff3c83f742d62daf73f36dbb582e
Quentin Sonnefraud <quentin.sonnefraud@protonmail.com>
2019-11-30 16:31:38 +0100

    [ADD]: add proxy

--- launch.sh
+++ launch.sh
@@ -1,7 +1,8 @@
 
 
-podman build --tag myhttp -f ./Dockerfile.http # Create image for http app
-podman build --tag myproxy -f ./Dockerfile.proxy # Create image for http proxy
-podman pod create -p 8080:80 --name mypod # Create pod and open on port 8080
-podman run -d --pod mypod myhttp # Run container in pod
-podman run -d --pod  mypod myproxy # Run container in pod
+sudo podman build --tag myhttp -f ./Dockerfile.http # Create image for http app
+sudo podman build --tag myproxy -f ./Dockerfile.nginx # Create image for http proxy
+sudo podman pod create -p 8080:80 --name mypod # Create pod and open on port 8080
+sudo podman run -d --pod mypod myhttp # Run container in pod
+sudo podman run -d --pod mypod myproxy # Run container in pod
+
