6024223c836e8f43925c2ed039e4dbd7c1311449
Quentin Sonnefraud <quentin.sonnefraud@protonmail.com>
2019-11-30 15:23:09 +0100

    [ADD]: add comment

--- launch.sh
+++ launch.sh
@@ -1,5 +1,7 @@
-sudo podman build --tag myhttp -f ./Dockerfile
-sudo podman build --tag myproxy -f ./Dockerfile.nginx
-sudo podman pod create -p 8080:80 --name mypod
-sudo podman run -d --pod mypod myhttp
-sudo podman run -d --pod  mypod myproxy
+
+
+podman build --tag myhttp -f ./Dockerfile.http # Create image for http app
+podman build --tag myproxy -f ./Dockerfile.proxy # Create image for http proxy
+podman pod create -p 8080:80 --name mypod # Create pod and open on port 8080
+podman run -d --pod mypod myhttp # Run container in pod
+podman run -d --pod  mypod myproxy # Run container in pod
